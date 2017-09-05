f4b2e79ffee6c93639573e65625b058b55d077af
Maggioxg9 <maggioxg9@aol.com>
2017-09-05 16:22:54 -0500

    update

---
+++ php_files/currentcount.php
@@ -0,0 +1,18 @@
+<?php
+	session_start();
+	
+	$ch = curl_init("https://api.density.io/v1");
+	$auth = "Authorization: Bearer " . $_SESSION['api_token'];
+	
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		$auth
+		));
+	
+	$response = curl_exec($ch);
+	$array = json_decode($response);
+	curl_close($ch);
+	
+	$_SESSION['currentcount'] = $array['current_count'];
+
+?>
