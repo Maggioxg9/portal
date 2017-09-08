57445f953c3d5bc37ef3be6fdad4b40563ef9386
Maggioxg9 <maggioxg9@aol.com>
2017-09-08 11:10:50 -0500

    update

--- php_files/currentcount.php
+++ php_files/currentcount.php
@@ -3,7 +3,7 @@
 	
 	$ch = curl_init('https://api.density.io/v1/');
 	$auth = "Authorization: Bearer tok_CcUbJqjQgYboQfYM4DuVHHIjdHuHFtjxlqeb0u9ZxCv";
-	$spcparam = "/spaces/spc_439902975167562685/count/"
+	$spcparam = "/spaces/spc_439902975167562685/count/";
 	
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
