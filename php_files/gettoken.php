24b47a53f69303311b089b26f48556e556dbaf5e
Maggioxg9 <maggioxg9@aol.com>
2017-09-11 10:39:51 -0500

    update

--- php_files/gettoken.php
+++ php_files/gettoken.php
@@ -2,14 +2,16 @@
 	session_start();
 	
 	$ch = curl_init('https://api.getdor.com/v1/tokens');
-	//$cont = "Content-Type: application/json";
-	$auth = "refresh_token:30cYae0ATX7JqGBnEdwf9FNwCn6lOv";
+	$cont = "Content-Type: application/json";
+	
+	$tok = array("refresh_token" => "30cYae0ATX7JqGBnEdwf9FNwCn6lOv");
+	$auth = json_encode($tok);
 	
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
 	curl_setopt($ch, CURLOPT_POST, 1);
-	//curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-	//	$cont
-	//	));
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		$cont
+		));
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $auth);
 	
 	$response = curl_exec($ch);
