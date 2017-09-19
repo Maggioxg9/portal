34d71936a5e6de9ac564d24b55cbcabc56a30408
Maggioxg9 <maggioxg9@aol.com>
2017-09-19 12:53:02 -0500

    update

--- jquery-calendar.js
+++ jquery-calendar.js
@@ -109,7 +109,7 @@
 									cell = $(row.cells[i]).find('div').remove();
 									cell = $(row.cells[i]).append('<div class="tdcaltext">'+dy+'</div>'); //add div for date text
 									cell = $(row.cells[i]).append('<div class="tdcalbody">'+Math.floor(Math.random()*100)+'</div>');
-									if((i+j)%8){
+									if(i%2&&j%3){
 											cell = $(row.cells[i]).append('<div class="tdcalnotes">marketing event started, had lot of people</div>');
 									}else{
 										cell = $(row.cells[i]).append('<div class="tdcalnotes"></div>');
